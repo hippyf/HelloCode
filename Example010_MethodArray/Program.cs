c5cc9ba829f4790cb75cf14218b77f3beb08101e
yana sarvarova <serg.yana@mail.ru>
2022-06-28 03:35:53 +0300

    Найти индекс заданного массива

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿//Найти индекс заданного элемента массива
+
+int[] array = {1, 12, 31, 4, 5, 6, 17, 18, 18};
+int n = array.Length; //количество элементов в массиве
+int find = 18;
+int index = 0;
+while(index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
